ca35b082233e2612b5ee99815532fbf168575994
nicolas <nicolas@uucidl.com>
2017-09-05 08:25:57 +0200

    Correct error checking during build

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+@setlocal
 @if not defined BuildDir @set BuildDir=builds
 @if not defined BuildObjDir @set BuildObjDir=%BuildDir%\obj
 @if not exist %BuildObjDir% mkdir %BuildObjDir%
@@ -17,10 +18,10 @@ cl -nologo -EHsc -Od -Z7 -W3 test_unit_uu_focus_main.cpp -Fo%BuildObjDir%\ ^
   -Fe%BuildDir%\test_uu_focus.exe
 echo TEST	test_uu_focus_exe
 %BuildDir%\test_uu_focus.exe --quiet >%BuildDir%\test_uu_focus.txt
-@if not errorlevel 0 (
+@if %ERRORLEVEL% neq 0 (
 	echo ERROR: test_uu_focus.exe
 	type %BuildDir%\test_uu_focus.txt
-	exit /b %errorlevel%
+	exit /b %ERRORLEVEL%
 )
 
 REM Build program:
@@ -37,7 +38,7 @@ cl -DUU_FOCUS_INTERNAL=1 ^
   -LD -link -PDB:%DllPdbPath% ^
   -EXPORT:win32_uu_focus_ui_render
 
-@if errorlevel 1 goto in_error_end
+@if %ERRORLEVEL% neq 0 goto in_error_end
 @REM only remove lock file on success, to indicate we have a working dll
 @attrib +R %DllPdbPath%
 @del %DllPrefixPath%_dll_*.pdb > NUL 2> NUL
@@ -46,29 +47,31 @@ cl -DUU_FOCUS_INTERNAL=1 ^
 
 rc.exe /nologo /fo %BuildDir%\uu_focus.res ^
    win32_unit_uu_focus_main.rc
-if not errorlevel 0 goto in_error_end
+if %ERRORLEVEL% neq 0 goto in_error_end
 
 cl -DUU_FOCUS_INTERNAL=1 ^
   -Fe%BuildDir%\uu_focus_dev.exe win32_unit_uu_focus_main.cpp ^
   %BuildDir%\uu_focus.res ^
   -Od -EHsc -Z7 -W3 -Fo%BuildObjDir%\ -nologo ^
   -link -PDB:%BuildDir%\uu_focus_dev.pdb
-if not errorlevel 0 goto in_error_end
+if %ERRORLEVEL% neq 0 goto in_error_end
 
 cl -DUU_FOCUS_INTERNAL=0 -DWIN32_WASAPI_SOUND_INTERNAL=0 ^
   -Fe%BuildDir%\uu_focus_release.exe win32_unit_uu_focus_main.cpp ^
   %BuildDir%\uu_focus.res ^
   -O2 -EHsc -Z7 -W3 -Fo%BuildObjDir%\ -nologo ^
   -link -PDB:%BuildDir%\uu_focus_release.pdb
-if not errorlevel 0 goto in_error_end
+if %ERRORLEVEL% neq 0 goto in_error_end
 
 mt.exe -nologo -manifest %BuildDir%\uu_focus_release.exe.manifest -outputresource:%BuildDir%\uu_focus_release.exe;1
-if not errorlevel 0 goto in_error_end
+if %ERRORLEVEL% neq 0 goto in_error_end
 
 copy %BuildDir%\uu_focus_release.exe %BuildDir%\uu_focus.exe
-if not errorlevel 0 goto in_error_end
+if %ERRORLEVEL% neq 0 goto in_error_end
 
+endlocal
 exit /b 0
 
 :in_error_end
+endlocal
 exit /b 1
